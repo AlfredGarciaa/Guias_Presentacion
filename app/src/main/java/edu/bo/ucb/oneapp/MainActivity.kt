610850f1172f16dd860377949c414efc34801438
Alfred <alfred.garcia@ucb.edu.bo>
2022-11-05 18:29:20 -0400

    Saber conexion a internet

--- app/src/main/java/edu/bo/ucb/oneapp/MainActivity.kt
+++ app/src/main/java/edu/bo/ucb/oneapp/MainActivity.kt
@@ -1,11 +1,40 @@
 package edu.bo.ucb.oneapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private val button: Button get() = findViewById(R.id.button)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+
+            if(isConexion(applicationContext)) {
+                Toast.makeText(this, "Tiene acceso a internet", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "No tiene acceso a internet", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    fun isConexion(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        val nw      = connectivityManager.activeNetwork ?: return false
+        val actNw = connectivityManager.getNetworkCapabilities(nw) ?: return false
+        return when {
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            //for other device how are able to connect with Ethernet
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            //for check internet over Bluetooth
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
+            else -> false
+        }
     }
 }
