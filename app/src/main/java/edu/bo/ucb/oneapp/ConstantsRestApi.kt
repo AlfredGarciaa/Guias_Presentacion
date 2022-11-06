337dae16f6ff3ad1eb0103d23c382cd03e19f1f5
Alfred <alfred.garcia@ucb.edu.bo>
2022-11-06 16:39:38 -0400

    Error en un parametro

---
+++ app/src/main/java/edu/bo/ucb/oneapp/ConstantsRestApi.kt
@@ -0,0 +1,8 @@
+package edu.bo.ucb.oneapp
+
+class ConstantsRestApi {
+    companion object {
+        const val URL_BASE = "https://jsonplaceholder.typicode.com"
+        const val POSTS ="/posts"
+    }
+}
---
+++ app/src/main/java/edu/bo/ucb/oneapp/Post.kt
@@ -0,0 +1,3 @@
+package edu.bo.ucb.oneapp
+
+data class Post(val userId: Int, val id: Int, val title: String, val body: String)
