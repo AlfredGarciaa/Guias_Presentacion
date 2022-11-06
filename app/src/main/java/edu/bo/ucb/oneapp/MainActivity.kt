620f8bf8ab85794d7f31f80ed761ec2829ba3204
Alfred <alfred.garcia@ucb.edu.bo>
2022-11-06 14:17:17 -0400

    Version 1

--- app/src/main/java/edu/bo/ucb/oneapp/MainActivity.kt
+++ app/src/main/java/edu/bo/ucb/oneapp/MainActivity.kt
@@ -1,11 +1,57 @@
 package edu.bo.ucb.oneapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val channelID = "channelID"
+    private val channelName = "channelName"
+
+    private val notificationId = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createNotificationChannel()
+
+        val notification = NotificationCompat.Builder(this, channelID).also {
+            it.setContentTitle("Titulo de notificacion")
+            it.setContentText("Este es el contenido de la notificacion")
+            it.setSmallIcon(R.drawable.ic_message)
+            it.setPriority(NotificationCompat.PRIORITY_HIGH)
+        }.build()
+
+        val notificationManager = NotificationManagerCompat.from(this)
+
+        val btn_newNotificacion = findViewById<View>(R.id.btn_newNotification) as Button
+        btn_newNotificacion.setOnClickListener {
+            notificationManager.notify(notificationId, notification)
+        }
+    }
+
+    private fun createNotificationChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_HIGH
+
+            val channel = NotificationChannel(channelID, channelName, importance).apply {
+                //LightColor = Color.RED
+                enableLights(true)
+            }
+
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            manager.createNotificationChannel(channel)
+        }
     }
 }
