42c9bb1336cfe1ad925b7bd8a9331bbbe8052b1f
Alfred <alfred.garcia@ucb.edu.bo>
2022-11-06 19:03:58 -0400

    Vista general

---
+++ app/src/main/java/edu/bo/ucb/oneapp/Utilidades.java
@@ -0,0 +1,14 @@
+package edu.bo.ucb.oneapp;
+
+/**
+ * Created by ALFRED G. on 06/11/2022.
+ */
+
+public class Utilidades {
+
+    public static final int LIST=1;
+    public static final int GRID=2;
+
+    public static int visualizacion=LIST;
+
+}
