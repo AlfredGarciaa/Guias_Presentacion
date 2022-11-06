a6ea88146034573a0b6ba1a2bc16f0b90de9d384
Alfred <alfred.garcia@ucb.edu.bo>
2022-11-06 16:07:11 -0400

    ROOM - Database

--- app/src/main/java/edu/bo/ucb/oneapp/MainActivity.kt
+++ app/src/main/java/edu/bo/ucb/oneapp/MainActivity.kt
@@ -2,10 +2,32 @@ package edu.bo.ucb.oneapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Mensaje -> buscar
+        val text = "Buscar    ->  !!! DBTEST en Logcat !!!"
+        val duration = Toast.LENGTH_LONG
+        Toast.makeText(applicationContext, text, duration).show()
+
+        GlobalScope.launch {
+            val bookDao = AppRoomDatabase.getDatabase(applicationContext).bookDato()
+            val repository = BookRepository(bookDao)
+            repository.insert(Book("the best seller: Android"))
+            val lista = repository.getListBooks()
+            lista.forEach {
+                Log.d("DBTEST","Id book = ${it.id}, Title: ${it.title}")
+            }
+        }
     }
 }
